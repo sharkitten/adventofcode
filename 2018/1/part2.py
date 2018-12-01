cab0d0079e44da2588b3b515369085685bda32f7
sharkitten <you@example.com>
2018-12-01 17:33:51 +0100

    speed improvements

--- 2018/1/part2.py
+++ 2018/1/part2.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
-frequencies = [0]
-i = 0
+seen = set([0])
+cur = i = 0
 
-changes = [int(x.strip()) for x in open("input.txt","r")]
-while len(set(frequencies)) == len(frequencies):
-    frequencies.append(frequencies[-1] + changes[i%len(changes)])
-    i+=1
-print(frequencies[-1])
+changes = [int(x) for x in open("input.txt","r")]
+l = len(changes)
+while True:
+    cur += changes[i%l]
+    i += 1
+    if cur in seen:
+        print(cur); break
+    seen.add(cur)
